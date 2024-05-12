8ead5cc94b4accc8e37a26750108fe583f516b1e
harsha-mydala <mharsha199625@gmail.com>
2024-05-12 19:36:16 -0400

    logistic regression added

---
+++ Supervised_ML/LogisticRegression/LogisticRegression.py
@@ -0,0 +1,20 @@
+import torch
+
+
+class LogisticRegression():
+
+    def __init__(self, lr=0.001, n_iters = 1000):
+
+        self.lr = lr
+        self.n_iters = n_iters
+
+    def sigmoid(self,x):
+
+        return 1/(1+torch.exp(-x))
+
+    def predict(self, X, y):
+
+        n_samples,n_features = X.shape[0], X.shape[1]
+
+        self.weights = torch.randn(n_features+1)
+        X = torch.cat(X,torch.ones(n_samples,1),axis = 1)
