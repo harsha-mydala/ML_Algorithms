23ec840e142798566b0b2d0c7951a7d4656455d1
harsha-mydala <mharsha199625@gmail.com>
2024-05-12 19:43:10 -0400

    conflict resolved

--- Supervised_ML/LogisticRegression/LogisticRegression.py
+++ Supervised_ML/LogisticRegression/LogisticRegression.py
@@ -12,9 +12,9 @@ class LogisticRegression():
 
         return 1/(1+torch.exp(-x))
 
-    def predictjithin(self, X, y):
+    def predictharsha(self, X, y):
 
         n_samples,n_features = X.shape[0], X.shape[1]
 
         self.weights = torch.randn(n_features+1)
-        X = torch.cat(X,torch.ones(n_samples,1),axis = 1)
+        # X = torch.cat(X,torch.ones(n_samples,1),axis = 1)
